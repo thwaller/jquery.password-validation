f053d93db15e4ea56fe206c1f9b9d6ec2252527d
thwaller <git@thwaller.com>
2020-12-04 10:02:48 -0600

    fix issues

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ JQuery plugin for password validation
 
 1. All UpperCase (Ascii/Unicode)  
    `upperCaseSet: "A-Z"`
-2. All LowerCase (Ascii/Unicode)
+2. All LowerCase (Ascii/Unicode)  
    `lowerCaseSet: "a-z"`
 3. All numeric (Ascii/Unicode)  
    `numericSet: "0-9"`
 4. All Other printable Ascii  
    `specialSet: "\\x20-\\x2F\\x3A-\\x40\\x5B-\\x60\\x7B-\\x7E\\x80-\\xFF"`
-5. the characters of space, double quote, single quote, slash, backslash, grave/accent  
+5. The characters of space, double quote, single quote, slash, backslash, grave/accent  
    `problemSet: "\\x20\\x22\\x27\\2F\\5C\\x60"`
 
--- jquery.password-validation.js
+++ jquery.password-validation.js
@@ -6,20 +6,21 @@
 				lowerCaseSet: "a-z",	//All LowerCase (Ascii/Unicode)
 				numericSet: "0-9",		//All numeric (Ascii/Unicode)
 				specialSet: "\\x20-\\x2F\\x3A-\\x40\\x5B-\\x60\\x7B-\\x7E\\x80-\\xFF", //All Other printable Ascii
-				problemSet: "\\x20\\x22\\x27\\2F\\5C\\x60", //the characters of space, double quote, single quote, slash, backslash, grave/accent
+				problemSet: "\\x20\\x22\\x27\\x2F\\x5C\\x60", //the characters of space, double quote, single quote, slash, backslash, grave/accent
 			}
 			var _defaults = {
 				minLength: 12,			//Minimum Length of password 
 				minUpperCase: 2,		//Minimum number of Upper Case Letters characters in password
 				minLowerCase: 2,		//Minimum number of Lower Case Letters characters in password
-				minDigits: 2,			//Minimum number of digits characters in password
+				minNumeric: 2,			//Minimum number of digits characters in password
 				minSpecial: 2,			//Minimum number of special characters in password
-				maxRepeats: 2,			//Maximum number of repeated alphanumeric characters in password dhgurAAAfjewd <- 3 A's
-				maxConsecutive: 3,		//Maximum number of alphanumeric characters from one set back to back
+				maxRepeats: 3,			//Maximum number of repeated alphanumeric characters in password dhgurAAAfjewd <- 3 A's
+				maxConsecutive: 1,		//Maximum number of alphanumeric characters from one set back to back
 				noUpper: false,			//Disallow Upper Case Letters
 				noLower: false,			//Disallow Lower Case Letters
-				noDigit: false,			//Disallow Digits
+				noNumeric: false,		//Disallow Numeric
 				noSpecial: false,		//Disallow Special Characters
+				noProblem: true,		//Disallow Problem set
 				failRepeats: true,		//Disallow user to have x number of repeated alphanumeric characters ex.. ..A..a..A.. <- fails if maxRepeats <= 3 CASE INSENSITIVE
 				failConsecutive: true,	//Disallow user to have x number of consecutive alphanumeric characters from any set ex.. abc <- fails if maxConsecutive <= 3
 				confirmField: undefined
@@ -42,14 +43,13 @@
 			if(_options.maxRepeats < 2) _options.maxRepeats = 2;
 
 			function charsetToString() {
-				return CHARSETS.upperCaseSet + CHARSETS.lowerCaseSet + CHARSETS.digitSet + CHARSETS.specialSet; 
+				return CHARSETS.upperCaseSet + CHARSETS.lowerCaseSet + CHARSETS.numericSet + CHARSETS.specialSet; 
 			}
 
 			//GENERATE ALL REGEXs FOR EVERY CASE
 			function buildPasswordRegex() {
 				var cases = [];
 
-				//if(_options.allowUnicode) CHARSETS.specialSet = _unicodeSpecialSet;
 				if(_options.noUpper) 	cases.push({"regex": "(?=" + CHARSETS.upperCaseSet + ")",
 													"message": "Password can't contain an Upper Case Letter"});
 				else 	cases.push({"regex": "(?=" + ("[" + CHARSETS.upperCaseSet + "][^" + CHARSETS.upperCaseSet + "]*").repeat(_options.minUpperCase) + ")",
@@ -60,24 +60,26 @@
 				else 	cases.push({"regex": "(?=" + ("[" + CHARSETS.lowerCaseSet + "][^" + CHARSETS.lowerCaseSet + "]*").repeat(_options.minLowerCase) + ")",
 									"message": "Password must contain at least " + _options.minLowerCase + " Lower Case Letters."});
 				
-				if(_options.noDigit) 	cases.push({"regex": "(?=" + CHARSETS.digitSet + ")",
+				if(_options.noNumeric) 	cases.push({"regex": "(?=" + CHARSETS.numericSet + ")",
 													"message": "Password can't contain a Number"});
-				else 	cases.push({"regex": "(?=" + ("[" + CHARSETS.numericSet + "][^" + CHARSETS.numericSet + "]*").repeat(_options.minDigits) + ")",
-									"message": "Password must contain at least " + _options.minDigits + " Digits."});
+				else 	cases.push({"regex": "(?=" + ("[" + CHARSETS.numericSet + "][^" + CHARSETS.numericSet + "]*").repeat(_options.minNumeric) + ")",
+									"message": "Password must contain at least " + _options.minNumeric + " Digits."});
 				
 				if(_options.noSpecial) 	cases.push({"regex": "(?=" + CHARSETS.specialSet + ")",
 													"message": "Password can't contain a Special Character"});
 				else 	cases.push({"regex": "(?=" + ("[" + CHARSETS.specialSet + "][^" + CHARSETS.specialSet + "]*").repeat(_options.minSpecial) + ")",
 									"message": "Password must contain at least " + _options.minSpecial + " Special Characters."});
 
+				//if(_options.noProblem) 	cases.push({"regex": "(?=" + CHARSETS.problemSet + ")",
+													//"message": "Password can't contain a space, double quote, single quote, slash, backslash or grave character"});
+
 				cases.push({"regex":"[" + charsetToString() + "]{" + _options.minLength + ",}",
-							"message":"Password must contain at least " + _options.minLength + " characters."});
+							"message":"Password must contain at least " + _options.minLength + " characters"});
 
 				return cases;
 			}
 
 			var _cases = buildPasswordRegex();
-
 			var _element = this;
 			var $confirmField = (_options.confirmField != undefined)? $(_options.confirmField): undefined;
 
